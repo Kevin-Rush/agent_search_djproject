7e220eb33fc2f05eb16e30dd64a35394a83e5e3f
Kevin Rush <kevinrush.net@gmail.com>
2024-08-08 15:09:34 +0200

    added labels to PromptForm

--- mysite/core/forms.py
+++ mysite/core/forms.py
@@ -13,4 +13,8 @@ class PromptForm(forms.ModelForm):
     class Meta:
         model = Prompt
         fields = ("user_prompt", "gpt_key")
+        labels = {
+            'user_prompt': 'User Prompt',
+            'gpt_key': 'GPT Key',
+        }
 
--- mysite/core/models.py
+++ mysite/core/models.py
@@ -19,6 +19,7 @@ from django.db import models
 class Prompt(models.Model):
     user_prompt = models.CharField(max_length=256)
     gpt_key = models.CharField(max_length=56)
+    search_result = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.user_prompt
