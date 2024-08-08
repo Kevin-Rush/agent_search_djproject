3fb0cf4b7c4f9c40ad024034811b4d7abd48d71c
Kevin Rush <kevinrush.net@gmail.com>
2024-08-08 15:11:45 +0200

    comment clean up

--- mysite/core/forms.py
+++ mysite/core/forms.py
@@ -1,13 +1,7 @@
 from django import forms
 
-# from .models import Book
 from .models import Prompt
 
-
-# class BookForm(forms.ModelForm):
-#     class Meta:
-#         model = Book
-#         fields = ('title', 'author', 'pdf', 'cover')
 
 class PromptForm(forms.ModelForm):
     class Meta:
--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, ListView, CreateView
+from django.views.generic import TemplateView
 
 from .forms import PromptForm
 from .models import Prompt
